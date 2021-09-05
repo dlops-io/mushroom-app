8e542f45387e24504ab8fade56c13f82d41e093d
Shivas Jayaram <shivasj@gmail.com>
2021-09-05 08:00:03 -0400

    Added code to download models

--- api-service/api/service.py
+++ api-service/api/service.py
@@ -1,5 +1,11 @@
+import asyncio
 from fastapi import FastAPI
 from starlette.middleware.cors import CORSMiddleware
+
+from api.tracker import TrackerService
+
+# Initialize Tracker Service
+tracker_service = TrackerService()
 
 # Setup FastAPI app
 app = FastAPI(
@@ -16,6 +22,19 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
+
+@app.on_event("startup")
+async def startup():
+    # Startup tasks
+    # Start the tracker service
+    asyncio.create_task(tracker_service.track())
+
+
+@app.on_event("shutdown")
+async def shutdown():
+    # Shutdown tasks
+    ...
 
 # Routes
 
