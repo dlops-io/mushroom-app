81f92c007c759eeee63e1d494690a29dc472d0bf
Shivas Jayaram <shivasj@gmail.com>
2021-11-08 10:24:48 -0500

    Deleted files

--- frontend-simple/docker-shell.bat
+++
@@ -1,5 +0,0 @@
-SET IMAGE_NAME=mushroom-app-frontend-simple
-SET BASE_DIR=%cd%
-
-docker build -t %IMAGE_NAME% -f Dockerfile .
-docker run  --rm --name %IMAGE_NAME% -ti --mount type=bind,source="%cd%",target=/app -p 8080:8080 %IMAGE_NAME%
