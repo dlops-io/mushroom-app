bcd232bfc0e4f88a67db4301ce4077e8945803c0
Shivas Jayaram <shivasj@gmail.com>
2021-11-13 15:11:05 -0500

    Deleted files

--- frontend-react/src/services/Common.js
+++
@@ -1,6 +0,0 @@
-export const BASE_API_URL = process.env.REACT_APP_BASE_API_URL;
-
-export function epochToJsDate(ts) {
-    let dt = new Date(ts)
-    return dt.toLocaleDateString() + " " + dt.toLocaleTimeString();
-}
